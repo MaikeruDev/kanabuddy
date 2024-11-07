da572e768904b214ed38dba32efcfad448b402e8
MaikeruDev <hubufanlp@gmail.com>
2024-11-07 13:07:14 +0100

    mobile padding

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import KanaBuddy from '@/components/kana-buddy'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-0 pt-24 sm:p-24">
       <KanaBuddy />
     </main>
   )
